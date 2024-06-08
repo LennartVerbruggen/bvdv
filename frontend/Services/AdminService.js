620ae2855c2771697377b9222e8647e8ef153549
Lennart Verbruggen <lennart.verbruggen@student.ucll.be>
2024-06-08 17:18:09 +0200

    change to https and 443 in order to test proxy config

--- frontend/Services/AdminService.js
+++ frontend/Services/AdminService.js
@@ -1,5 +1,5 @@
 const getAllGroups = async () => {
-    return await fetch(`http://158.180.14.4:3000/admin/groups`, {
+    return await fetch(`https://158.180.14.4:443/admin/groups`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -8,7 +8,7 @@ const getAllGroups = async () => {
 }
 
 const setActive = async (groep) => {
-    return await fetch(`http://158.180.14.4:3000/admin/activate/`, {
+    return await fetch(`https://158.180.14.4:443/admin/activate/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -19,7 +19,7 @@ const setActive = async (groep) => {
 }
 
 const createGroep = async (groep) => {
-    return await fetch(`http://158.180.14.4:3000/admin/create/`, {
+    return await fetch(`https://158.180.14.4:443/admin/create/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -31,7 +31,7 @@ const createGroep = async (groep) => {
 }
 
 const deleteGroep = async (groep) => {
-    return await fetch(`http://158.180.14.4:3000/admin/delete`, {
+    return await fetch(`https://158.180.14.4:443/admin/delete`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
@@ -43,7 +43,7 @@ const deleteGroep = async (groep) => {
 }
 
 const generateStatistics = async (groep) => {
-    return await fetch(`http://158.180.14.4:3000/admin/statistics/${groep}`, {
+    return await fetch(`https://158.180.14.4:443/admin/statistics/${groep}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -52,7 +52,7 @@ const generateStatistics = async (groep) => {
 }
 
 const getActive = async () => {
-    return await fetch(`http://158.180.14.4:3000/admin/activegroep`, {
+    return await fetch(`https://158.180.14.4:443/admin/activegroep`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
