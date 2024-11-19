82cf2eeef41b4d71bb51ffddb71102a0b2e61b75
Lennart Verbruggen <lennart.verbruggen@student.ucll.be>
2024-11-19 10:34:04 +0100

    added cronjob to send excel every other day

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const express = require('express');
+const cron = require('node-cron');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const fs = require('fs');
 const cors = require('cors');
 const router = express.Router();
 const swaggerSetup = require('./swagger');
@@ -11,6 +15,54 @@ const adminRouter = require('./routes/admin');
 var bodyParser = require('body-parser')
 
 const app = express();
+
+// Function to send an email with the Excel file
+const sendEmail = async () => {
+
+  const filePath = path.join(__dirname, 'Data/data.xlsx');
+
+  // Read the file
+  const fileContent = fs.readFileSync(filePath);
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // or 'STARTTLS'
+    auth: {
+      user: 'bvdvapp@gmail.com',
+      pass: 'iyjiqllmpqavvyxe',
+    },
+  });
+
+  const mailOptions = {
+    from: 'bvdvapp@gmail.com',
+    to: 'lennart.verbruggen@hotmail.com, toon.van.criekinge@gmail.com',
+    subject: 'Daily Status Report',
+    text: 'Please find the attached status report.',
+    attachments: [
+      {
+        filename: 'data.xlsx',
+        content: fileContent,
+        contentType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+      },
+    ],
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
+
+// Schedule the task to run every day at 8:00 AM
+cron.schedule('0 10 */2 * *', async () => {
+  console.log('Running daily status update task');
+  sendEmail();
+});
+
+console.log('Cron job scheduled');
 
 const options = {
     definition: {
